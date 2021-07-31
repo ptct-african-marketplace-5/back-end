f9215edf74a964aeec022882793166781436cd99
yonathan Admasu <admyoni4@yahoo.com>
2021-07-31 13:11:07 -0700

    created an api test for items

--- api/__tests__/tests.test.js
+++ api/__tests__/tests.test.js
@@ -1,8 +1,7 @@
-const request = require('supertest')
-const server = require('../server.js')
+const request = require('supertest')//lets us do server calls
 const db = require('../data/db-config')
-const bcrypt = require('bcryptjs')
-const jwtDecode = require('jwt-decode')
+const server = require('../server')// supertest needs access to our server
+const Item = require('./')
 
 beforeAll(async () => {
     await db.migrate.rollback()
@@ -19,9 +18,72 @@ it('sanity check', () => {
     expect(true).not.toBe(false)
 })
 
-describe('[POST] /api/auth/login', () => {
-    it('[1] responds with the correct message on valid credentials', async () => {
-        const res = await request(server).post('/api/auth/login').send({ username: 'gabrielhenton', password: 'password' })
-        expect(res.body.message).toMatch(/gabrielhenton is back/i)
+
+const item1 = {
+    item_name: "Milk",
+    image: "https://i.imgur.com/4iRINNJ.jpg",
+    description: "Delicious milk from cows that roam free",
+    category_id: 5,
+    location_id: 1,
+    user_id: 2
+}
+
+const item2 = {
+    item_name: "Honey",
+    image: "https://i.imgur.com/L65zYph.jpg",
+    description: "Raw Honey",
+    category_id: 12,
+    location_id: 1,
+    user_id: 2
+}
+
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
+
+beforeEach(async () => {
+    await db("items").truncate()
+})
+
+afterAll(async () => {
+    await db.destroy()
+})
+
+it("correct env var", () => {
+    expect(process.env.DB_ENV).toBe("testing")
+})
+
+describe("items model functions", () => {
+    describe("create item", () => {
+        it("adds item to the db", async () => {
+            let items
+            await Item.add(item1)
+            items = await db("items")
+            expect(items).toHaveLength(1)
+
+            await Item.add(item2)
+            items = await db("items")
+            expect(items).toHaveLength(2)
+        })
+        it("inserted a item and a description", async () => {
+            const item = await Item.add(item1)//before each "it" it destroys the db so
+            expect(item).toMatchObject({item_id: 1,...item})
+        })
+    })
+    describe("[DELETE] / - delete item", () => {
+        it("removes item from db", async () => {
+            const [item_id] = await db("items").insert(item1)
+            let item = await db("items").where({item_id}).first()//same as ({"item_id", item_id})
+            expect(item).toBeTruthy()
+            await request(server).delete("/items/"+ item_id)// supertest library
+            item = await db("items").where({item_id}).first()
+            expect(item).toBeFalsy()
+        })
+        it("respond with the deleted item", async () => {
+            await db("items").insert(item1)
+            let item = await request(server).delete("/items/1")
+            expect(item.body).toMatchObject(item1)
+        })
     })
 })
